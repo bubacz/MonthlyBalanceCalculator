09412c7f0a24255a7c9fef8c21f9e57f69fd027a
crunchyk989 <kevinb989@gmail.com>
2016-03-06 18:47:42 -0500

    added string library JS file

---
+++ string_library.js
@@ -0,0 +1,36 @@
+/*
+Kevin Bubacz
+WEB115
+02/26/2016
+*/
+
+var $ = function(id) { return document.getElementById(id); }
+
+var pad_left = function(text, width, pad) {
+    if ( arguments.length < 2 || arguments.length > 3 ) {
+        return "";
+    }
+    if ( arguments.length == 2 ) {
+        pad = " ";
+    }
+    var result = text.toString();
+    while ( result.length < width ) {
+        result = pad + result;
+    }
+    return result;
+}
+
+var pad_right = function(text, width, pad) {
+    if ( arguments.length < 2 || arguments.length > 3 ) {
+        return "";
+    }
+    if ( arguments.length == 2 ) {
+        pad = " ";
+    }
+    
+    var result = text.toString();
+    while ( result.length < width ) {
+        result = result + pad;
+    }
+    return result;
+}
